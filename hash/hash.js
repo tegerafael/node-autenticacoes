48783f5efe21c02437a52ce58ec4af13effd2b35
Tiago Rafael <tiagorafael777@gmail.com>
2023-04-28 14:07:36 -0400

    hash finalizado

--- hash/hash.js
+++ hash/hash.js
@@ -1,1 +1,34 @@
 import { createHash } from "crypto";
+
+function criaHash(senha) {
+  return createHash("sha256").update(senha).digest("hex");
+}
+
+console.log(criaHash("uma String Qualquer"));
+
+class Usuario {
+  constructor(nome, senha) {
+    this.nome = nome;
+    this.hash = criaHash(senha);
+  }
+
+  autentica(nome, senha) {
+    if (nome === this.nome && this.hash == criaHash(senha)) {
+      console.log("Usuário cadastrado com sucesso");
+      return true;
+    } else {
+      console.log("Usuário ou senha incorretos");
+    }
+  }
+}
+
+const usuario = new Usuario("Tiago Rafael", "minhaSenha");
+
+console.log(usuario);
+
+// caso sucesso
+usuario.autentica("Tiago Rafael", "minhaSenha");
+
+// caso de fracasso
+usuario.autentica("Tiago", "minhaSenha");
+usuario.autentica("Tiago", "minhasenha");
